cdeb20eaa3cb8cd175d10df691b37f169815115d
Vladimir Ivanov <vladimir.ivanov@socialsweethearts.de>
2021-03-20 16:48:35 +0100

    add list posts api endpoint

--- server/index.js
+++ server/index.js
@@ -2,11 +2,26 @@
 
 const Hapi = require('@hapi/hapi');
 
+
+let posts = [
+    { id: 1, content: 'First post' },
+    { id: 2, content: 'Second post' },
+    { id: 3, content: 'Third post' },
+];
+
 const init = async () => {
 
     const server = Hapi.server({
         port: 3010,
         host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/posts/',
+        handler: (request, h) => {
+            return posts;
+        }
     });
 
     await server.start();
